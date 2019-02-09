6b01070de5b5a6164545639fd7ad51d309c2383d
chinghancheng <helen3909@gmail.com>
2019-02-09 10:35:47 +0000

    Chapter9

--- rango/views.py
+++ rango/views.py
@@ -178,9 +178,11 @@ def user_login(request):
         # will raise a KeyError exception.
         username = request.POST.get('username')
         password = request.POST.get('password')
+
         # Use Django's machinery to attempt to see if the username/password
         # combination is valid - a User object is returned if it is.
         user = authenticate(username=username, password=password)
+
         # If we have a User object, the details are correct.
         # If None (Python's way of representing the absence of a value), no user
         # with matching credentials was found.
@@ -193,12 +195,14 @@ def user_login(request):
                 return HttpResponseRedirect(reverse('index'))
             else:
                 # An inactive account was used - no logging in!
-                return HttpResponse("Your Rango account is disabled.")
+                # return HttpResponse("Your Rango account is disabled.")
+                return render(request, "rango/restricted.html", {"msg": "Your Rango account is disabled."})
         else:
             # Bad login details were provided. So we can't log the user in.
             print("Invalid login details: {0}, {1}".format(username, password))
-            return HttpResponse("Invalid login details supplied.")
-     # The request is not a HTTP POST, so display the login form.
+            # return HttpResponse("Invalid login details supplied.")
+            return render(request, "rango/restricted.html", {"msg": "Invalid login details supplied."})
+    # The request is not a HTTP POST, so display the login form.
     # This scenario would most likely be a HTTP GET.
     else:
         # No context variables to pass to the template system, hence the
